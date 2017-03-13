09dcd8fdc5b05f6339c8e9869f4c621c97d1d3a8
Adria Auguets <adriiauguets@gmail.com>
2017-03-13 13:36:52 +0100

    AUPAAA JA VAAAAA

--- Practica1/GAprimer.c
+++ Practica1/GAprimer.c
@@ -4,12 +4,13 @@
 #include "control_TMR0.h"
 
 #include <avr/interrupt.h>
+#include <stdio.h> //pel serial
 
-static uint32_t s_1[8]; //s-1
-static uint32_t s_2[8]; //s-2
-static uint8_t b[8] = {436,419,400,380,298,258,202,143}; //calculats amb l'octave. octave_b.png
+static int32_t s_1[8]; //s-1
+static int32_t s_2[8]; //s-2
+static int16_t b[8] = {436,419,400,380,298,258,202,143}; //calculats amb l'octave. octave_b.png
 static volatile int n = 0; //comptador per saber quin calcul de goertzel s'ha de fer.
-static uint32_t potencies[8];
+static int32_t potencies[8];
 
 
 static int uart_putchar(char c, FILE *stream);
@@ -25,8 +26,9 @@ static int uart_putchar(char c, FILE *stream){
 
 static void calcula_potencia(void){
   for(int i=0; i<8; i++){
-    if(((int32_t)s_1[i]*s_1[i] + (int32_t)s_2[i]*s_2[i] - (((int32_t)b[i]*s_1[i])>>8) * s_2[i])>10000){
-      printf("OK.");
+    potencies[i]=((int32_t)s_1[i]*s_1[i] + (int32_t)s_2[i]*s_2[i] - ((((int32_t)b[i]*s_1[i])>>8) * s_2[i]));
+    if(potencies[i]>9000000){
+      printf("p:%li-%d\n",potencies[i],i);
     }
   }
 }
@@ -36,22 +38,29 @@ void setup(){
   //adc_input (0-5 (default=5),8 Tª, 14 1.1V, 15 GND
   //v_ref 0 (AREF), 1(1.1V), default=5 (5V)
   //adc_pre 2,4,8,16(default),32,64,128
-  setup_tmr0(99,8);//(ocr0a, tmr0_pre)
+  setup_tmr0(249,8);//(ocr0a, tmr0_pre)
   //tmr0_pre 1,default=8,64,256,1024
   //TMR0=prescaler*(ocr0a+1)*T_clk
-
+  start_ADC();
   DDRD |=(1<<DDD4);//pin 4 Arduino as an output. It shows sampling period (period) and ISR execution time (pulse wide)
+  serial_init();
   sei();
 }
 
 int main(void){
   setup();
-  sei();
   stdout = &mystdout;
   while(1){
-    while (n < 204);
-    calcula_potencia();
-
+    if(n > 204){
+    //cli();
+      calcula_potencia();
+      n=0;
+      for (int i =0; i<8; i++){
+        s_1[i]=0;
+        s_2[i]=0;
+      }
+    }
+    //sei();
   }
 }
 
@@ -60,26 +69,26 @@ ISR(TIMER0_COMPA_vect){
    uint8_t value=read8_ADC();
    start_ADC();
    //Goertzel valors.
-   uint16_t s;
+   int16_t s;
    //s(1)=x(1);
    if (n == 0){
-     for(int = 0; i<8; i++){
+     for(int i = 0; i<8; i++){
        s_1[i] = value;}
    }
 
    //s(2)=x(2)+2*cos(wo)*s(1);
    else if (n == 1){
      for(int i=0; i<8; i++){
-       s_2[i] = value + ((int32_t)b[i]*s_1>>8)
+       s_2[i] = value + ((int32_t)b[i]*s_1[i]>>8);
      }
    }
    //s(n)=x(n)+2*cos(wo)*s(n-1)-s(n-2);
    //tractar el primer bucle aqui del octave.
    else{
      for(int i=0; i<8; i++){
-       s = value + (((int32_t)b[i]*s_1[i])>>8)-s_2[i]
-       s_2 = s_1[i]
-       s_1 = s
+       s = value + (((int32_t)b[i]*s_1[i])>>8)-s_2[i];
+       s_2[i] = s_1[i];
+       s_1[i] = s;
      }
    }
    n++;
